8815fccbd7766b463e62166878bbc083b3f19da2
wallyson.souza <wallyson.souza@FULL-101>
2024-01-15 09:19:24 -0300

    - Classe "Position" criada. - Arquivo .gitignore adicionado.

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Board;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position;
+
+            position = new Position(3, 4);
+
+            Console.WriteLine(position);
+        }
+    }
+}
