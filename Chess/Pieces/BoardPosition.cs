dd96e94a901c1a57b84640cea2383e7a01807899
wallyson.souza <wallyson.souza@FULL-101>
2024-01-15 11:23:11 -0300

    - Classe BoardPosition criada.

---
+++ Chess/Pieces/BoardPosition.cs
@@ -0,0 +1,25 @@
+﻿using BoardNS;
+
+namespace Pieces
+{
+    class BoardPosition
+    {
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        public BoardPosition(char column, int row)
+        {
+            Row = row;
+            Column = column;
+        }
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+        public override string ToString()
+        {
+            return Row.ToString() + Column.ToString();
+        }
+
+    }
+}
--- Chess/Program.cs
+++ Chess/Program.cs
@@ -9,19 +9,9 @@ namespace Chess
         {
             try
             {
-                Position position = new Position(3, 4);
-                Position position2 = new Position(3, 9);
+                BoardPosition position = new BoardPosition('c', 7);
+                Console.WriteLine(position.ToPosition());
 
-
-                Board board = new Board(8, 8);
-                Rook rook = new Rook(board, Color.White);
-                King king = new King(board, Color.White);
-
-
-                board.SetPiece(rook, position);
-                board.SetPiece(king, position2);
-
-                Display.PrintBoard(board);
 
             } catch(Exception ex)
             {
